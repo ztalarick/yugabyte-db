53e5208a6c409f32f0cd58782be8f93582840a0c
Bharat Kumar <bkumar@yugabyte.com>
2022-02-15 04:03:01 +0000

    added more commands

--- managed/devops/ami_preprovision.sh
+++ managed/devops/ami_preprovision.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-yum install -y git
+sudo yum install -y git postgresql-devel openldap-devel
 git clone https://github.com/yugabyte/yugabyte-db
+set -x
+export USER=root
 yugabyte-db/managed/devops/bin/install_python_requirements.sh
 yugabyte-db/managed/devops/bin/install_ansible_requirements.sh
