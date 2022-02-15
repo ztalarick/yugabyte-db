5f2956716725153f596d036cbc6458d0942cfc24
Bharat Kumar <bkumar@yugabyte.com>
2022-02-15 18:56:10 +0000

    Removed the need for ami_preprovision.sh

--- managed/devops/ami_preprovision.sh
+++ managed/devops/ami_preprovision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install -y git postgresql-devel openldap-devel
+sudo yum install -y git postgresql-devel openldap-devel python3 gcc python3-devel
 git clone https://github.com/yugabyte/yugabyte-db
 set -x
 export USER=root
