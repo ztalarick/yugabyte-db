c8f402d46fcd35c2cde8bf160eb6beab980e14a9
Jagdish Shinde <jshinde@yugabyte.com>
2022-04-28 07:03:18 +0000

    file utils for reading test resources

---
+++ managed/src/main/java/com/yugabyte/yw/models/helpers/FileUtils.java
@@ -0,0 +1,26 @@
+package com.yugabyte.yw.models.helpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+
+import play.Environment;
+
+public class FileUtils {
+
+  public static String fileContentsAsString(String filePath, Environment environment) {
+    if (filePath == null || filePath.isEmpty()) {
+      throw new IllegalArgumentException("File path can't be null or empty");
+    }
+    String fileContent;
+    try (InputStream inputStream = environment.resourceAsStream(filePath)) {
+      fileContent = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to read file " + filePath, e);
+    }
+
+    return fileContent;
+  }
+}
