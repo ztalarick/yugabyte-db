5b2c0e7cc98b26db73fc13ad61435129b99c18a8
Athul Sathyan <44346129+athulsn@users.noreply.github.com>
2022-06-28 16:36:43 +0530

    [PLAT-4093] [PLATFORM][UI] Set up new routes and basic placeholder containers
    Summary:
      # Created new routes for universe form
      # Setup folder structure
      # Added few basic containers
    
    Folder Structure
    {F27050}
    
    Test Plan: Tested manually
    
    Reviewers: ssutar, mjoshi, kkannan, cpadinjareveettil, lsangappa
    
    Reviewed By: kkannan, cpadinjareveettil, lsangappa
    
    Subscribers: jenkins-bot, ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17973

---
+++ managed/ui/src/redesign/features/universe/universe-form/UniverseForm.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+interface UniverseFormProps {}
+
+export const UniverseForm: FC<UniverseFormProps> = () => {
+  return <>Universe Form (UNDER DEV)</>;
+};
---
+++ managed/ui/src/redesign/features/universe/universe-form/cloudConfig/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+interface CloudConfigProps {}
+
+export const CloudConfig: FC<CloudConfigProps> = () => {
+  return <>Cloud Config (UNDER DEV)</>;
+};
