c3e2c52d832e4ff731a1a3f4db07a84875caf35b
Athul Sathyan <44346129+athulsn@users.noreply.github.com>
2022-08-02 15:53:24 +0530

    [PLAT-4752] [PLATFORM][UI] - Conditional Rendering
    Summary:
      - Conditional rendering of form fields
      - Added a global context provider to the form
      - Fixed minor ui issue
    
    Test Plan: Tested manually
    
    Reviewers: kkannan, cpadinjareveettil, ssutar, mjoshi, lsangappa
    
    Reviewed By: lsangappa
    
    Subscribers: jenkins-bot, ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18677

--- managed/ui/src/redesign/features/universe/universe-form/sections/advanced/AdvancedConfiguration.tsx
+++ managed/ui/src/redesign/features/universe/universe-form/sections/advanced/AdvancedConfiguration.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useWatch } from 'react-hook-form';
 import { Box, Grid, Typography } from '@material-ui/core';
 import { useSectionStyles } from '../../universeMainStyle';
 import {
@@ -12,12 +14,29 @@ import {
   SystemDField,
   YBCField
 } from '../../fields';
+import { PROVIDER_FIELD } from '../../utils/constants';
+import { CloudType, clusterModes } from '../../utils/dto';
+import { UniverseFormContext } from '../../UniverseForm';
 
 interface AdvancedConfigProps {}
 
 export const AdvancedConfiguration: FC<AdvancedConfigProps> = () => {
   const classes = useSectionStyles();
   const { t } = useTranslation();
+
+  //feature flagging
+  const featureFlags = useSelector((state: any) => state.featureFlags);
+  const isYBCEnabled = featureFlags.test.enableYbc || featureFlags.released.enableYbc;
+
+  //form context
+  const { isPrimary, mode } = useContext(UniverseFormContext);
+  const isFieldReadOnly = mode === clusterModes.EDIT_PRIMARY;
+  const isDbVersionReadOnly = mode !== clusterModes.NEW_PRIMARY;
+
+  //field data
+  const provider = useWatch({ name: PROVIDER_FIELD });
+
+  if (!provider?.code) return null;
 
   return (
     <Box className={classes.sectionContainer}>
@@ -26,62 +45,74 @@ export const AdvancedConfiguration: FC<AdvancedConfigProps> = () => {
         <Box mt={2}>
           <Grid container spacing={3}>
             <Grid lg={6} item container>
-              <DBVersionField disabled={false} />
+              <DBVersionField disabled={isDbVersionReadOnly} />
             </Grid>
 
-            <Grid lg={6} item container>
-              <AccessKeysField disabled={false} />
-            </Grid>
+            {provider.code !== CloudType.kubernetes && (
+              <Grid lg={6} item container>
+                <AccessKeysField disabled={isFieldReadOnly} />
+              </Grid>
+            )}
           </Grid>
         </Box>
 
-        <Box mt={2}>
-          <Grid container spacing={3}>
-            <Grid lg={6} item container>
-              <YBCField disabled={false} />
+        {isYBCEnabled && (
+          <Box mt={2}>
+            <Grid container spacing={3}>
+              <Grid lg={6} item container>
+                <YBCField disabled={isFieldReadOnly} />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        )}
 
-        <Box mt={2}>
-          <Grid container spacing={3}>
-            <Grid lg={6} item container>
-              <ARNField disabled={false} />
+        {provider.code === CloudType.aws && (
+          <Box mt={2}>
+            <Grid container spacing={3}>
+              <Grid lg={6} item container>
+                <ARNField disabled={isFieldReadOnly} />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        )}
+
+        {provider.code === CloudType.kubernetes && (
+          <>
+            <Box mt={2}>
+              <Grid container>
+                <Grid lg={6} item container>
+                  <IPV6Field disabled={isFieldReadOnly || !isPrimary} />
+                </Grid>
+              </Grid>
+            </Box>
+
+            <Box mt={2}>
+              <Grid container>
+                <Grid lg={6} item container>
+                  <NetworkAccessField disabled={isFieldReadOnly || !isPrimary} />
+                </Grid>
+              </Grid>
+            </Box>
+          </>
+        )}
 
         <Box mt={2}>
           <Grid container>
             <Grid lg={6} item container>
-              <IPV6Field disabled={false} />
+              <SystemDField disabled={isFieldReadOnly || !isPrimary} />
             </Grid>
           </Grid>
         </Box>
 
-        <Box mt={2}>
-          <Grid container>
-            <Grid lg={6} item container>
-              <NetworkAccessField disabled={false} />
+        {provider.code !== CloudType.kubernetes && (
+          <Box mt={2}>
+            <Grid container>
+              <Grid lg={6} item container>
+                <DeploymentPortsField disabled={isFieldReadOnly || !isPrimary} />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-
-        <Box mt={2}>
-          <Grid container>
-            <Grid lg={6} item container>
-              <SystemDField disabled={false} />
-            </Grid>
-          </Grid>
-        </Box>
-
-        <Box mt={2}>
-          <Grid container>
-            <Grid lg={6} item container>
-              <DeploymentPortsField disabled={false} />
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        )}
       </Box>
     </Box>
   );
