971e64d393f54594d3ba00e47e758062d6b5ae77
lingeshwar <lingesh100@gmail.com>
2022-11-19 14:07:21 +0530

    [PLAT-6232] - feat :changed new cluster creation design flow
    Summary:
    1. Changed new cluster creation design from single form to multiple instances.
    2. Fixed other flows.
    3. Fixed Instance type field by selecting default values from form when it exists
    
    Test Plan: Tested Manually
    
    Reviewers: kkannan, asathyan
    
    Reviewed By: asathyan
    
    Subscribers: jenkins-bot, yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21238

--- managed/ui/src/redesign/features/universe/universe-form/cluster-operations/CreateUniverse.tsx
+++ managed/ui/src/redesign/features/universe/universe-form/cluster-operations/CreateUniverse.tsx
@@ -5,14 +5,19 @@ import { UniverseForm } from '../UniverseForm';
 import { ClusterType, ClusterModes, DEFAULT_FORM_DATA, UniverseFormData } from '../utils/dto';
 import { UniverseFormContext } from '../UniverseFormContainer';
 import { createUniverse } from '../utils/helpers';
+import { useUpdateEffect } from 'react-use';
 
 interface CreateUniverseProps {}
 
 export const CreateUniverse: FC<CreateUniverseProps> = () => {
   const { t } = useTranslation();
   const [state, formMethods] = useContext(UniverseFormContext);
-  const { isLoading } = state;
+  const { isLoading, PrimaryFormData, AsyncFormData, clusterType } = state;
   const featureFlags = useSelector((state: any) => state.featureFlags);
+
+  const onSubmit = (formData: UniverseFormData) => {
+    createUniverse({ formData, universeContextData: state, featureFlags });
+  };
 
   useEffect(() => {
     formMethods.initializeForm({
@@ -21,23 +26,46 @@ export const CreateUniverse: FC<CreateUniverseProps> = () => {
     });
   }, []);
 
-  const onSubmit = (formData: UniverseFormData) => {
-    createUniverse({ formData, universeContextData: state, featureFlags });
-  };
+  useUpdateEffect(() => {
+    formMethods.toggleClusterType(ClusterType.ASYNC);
+  }, [PrimaryFormData]);
+
+  useUpdateEffect(() => {
+    formMethods.toggleClusterType(ClusterType.PRIMARY);
+  }, [AsyncFormData]);
+
+  useUpdateEffect(() => {
+    formMethods.setLoader(false);
+  }, [clusterType]);
 
   if (isLoading) return <>Loading .... </>;
 
-  return (
-    <UniverseForm
-      defaultFormData={DEFAULT_FORM_DATA}
-      title={
-        state.clusterType === ClusterType.PRIMARY
-          ? t('universeForm.createUniverse')
-          : t('universeForm.configReadReplica')
-      }
-      onFormSubmit={(data: UniverseFormData) => onSubmit(data)}
-      onCancel={() => console.log('cancelled')}
-      isPrimaryAndRR={true}
-    />
-  );
+  if (state.clusterType === ClusterType.PRIMARY)
+    return (
+      <UniverseForm
+        defaultFormData={PrimaryFormData ?? DEFAULT_FORM_DATA}
+        title={t('universeForm.createUniverse')}
+        onFormSubmit={(data: UniverseFormData) => onSubmit(data)}
+        onCancel={() => console.log('cancelled')}
+        onClusterTypeChange={(data: UniverseFormData) => {
+          formMethods.setLoader(true);
+          formMethods.setPrimaryFormData(data);
+        }}
+        key={ClusterType.PRIMARY}
+      />
+    );
+  else
+    return (
+      <UniverseForm
+        defaultFormData={AsyncFormData ?? PrimaryFormData}
+        title={t('universeForm.configReadReplica')}
+        onFormSubmit={(data: UniverseFormData) => console.log(data)}
+        onCancel={() => console.log('cancelled')}
+        onClusterTypeChange={(data: UniverseFormData) => {
+          formMethods.setLoader(true);
+          formMethods.setAsyncFormData(data);
+        }}
+        key={ClusterType.ASYNC}
+      />
+    );
 };
