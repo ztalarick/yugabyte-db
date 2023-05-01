4848b0e7c415aa4090491019e5b455a81823a338
RKN Sai Krishna <srangavajjula@yugabyte.com>
2023-05-01 19:13:55 +0530

    Bug fix

--- src/yb/client/batcher.cc
+++ src/yb/client/batcher.cc
@@ -513,7 +513,8 @@ void Batcher::AllLookupsDone() {
   }
   ops_info_.groups.emplace_back(group_start, ops_queue_.end());
 
-  if (this->transaction() && !session->IsDDLMode() && session->IsSingleShardConversion()) {
+  if (this->transaction() && session && !session->IsDDLMode() &&
+      session->IsSingleShardConversion()) {
     if (session->GetNumTabletsInvolvedInTxn() == 1) {
       session->batcher_config_.transaction = nullptr;
       this->transaction_ = nullptr;
